0ceb820b3e80922d5c790bd2bec903d96962c52b
John Catterfeld <jcatterfeld@opentable.com>
2014-02-22 20:09:21 +0000

    Added CSSLint, FTPDeploy and Shell.  Moved generated, production CSS and JS files.

--- gruntfile.js
+++ gruntfile.js
@@ -27,39 +27,78 @@ module.exports = function(grunt) {
 			},
 			combine: {
 				files: {
-					'css/compiled/all.min.css': 'css/compiled/all.css'
+					'css/all.min.css': 'css/compiled/all.css'
 				}
 			}
 		},
 		concat: {
 			combine: {
 				files: {
-					'js/compiled/all.js': ['js/lib/*.js', 'js/*.js', 'js/controls/*.js']
+					'js/compiled/all.js': ['js/lib/*.js', 'js/*.js', '!js/*.min.js']
 				}			
 			}
 		},
 		uglify: {
 			combine: {
 				files: {
-					'js/compiled/all.min.js': ['js/compiled/all.js']
+					'js/all.min.js': ['js/compiled/all.js']
 				}
 			}
 		},
+		csslint: {
+			options: {
+				csslintrc: '.csslintrc'
+			},
+			src: ['css/compiled/all.css']
+		},
 		watch: {
 			all: {
-				files: ['**/*.less', '**/*.js', '!js/compiled/*.js'],
+				files: ['**/*.less', '**/*.js', '!js/all.min.js'],
 				tasks: ['less', 'cssmin', 'concat', 'uglify']
 			}
-		}
+		},
+		'ftp-deploy': {
+	        live: {
+				auth: {
+					host: 'example.com',
+					port: 21,
+					authKey: 'accountName'
+				},
+    			exclusions: ['node_modules', '.git*', 'gruntfile.js', '.ftppass', '.csslintrc', 'package.json', '*.less', 'compiled', 'js/lib', 'web.config'],
+				src: 'C:/\Users/\John Catterfeld/\Dropbox/\Websites/\html5-boilerplate',
+				dest: '/public_html/'
+			},
+	        dev: {
+				auth: {
+					host: 'example.com',
+					port: 21,
+					authKey: 'accountName'
+				},
+    			exclusions: ['node_modules', '.git*', 'gruntfile.js', '.ftppass', '.csslintrc', 'package.json', '*.less', 'compiled', 'js/lib', 'web.config'],
+				src: 'C:/\Users/\John Catterfeld/\Dropbox/\Websites/\html5-boilerplate',
+				dest: '/development/'
+			}
+		},
+	    shell: {
+	        'git-push': {
+	            command: 'git push'
+	        }
+	    }
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-csslint');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-ftp-deploy');
+	grunt.loadNpmTasks('grunt-shell');
 
-	grunt.registerTask('dev', ['less', 'cssmin', 'concat', 'uglify', 'watch']);
 	grunt.registerTask('default', ['less', 'cssmin', 'concat', 'uglify']);
-
-};
+	grunt.registerTask('dev', ['less', 'cssmin', 'concat', 'uglify', 'watch']);
+	grunt.registerTask('lint', ['csslint']);
+	grunt.registerTask('push', ['shell:git-push']);
+	grunt.registerTask('deploy-dev', ['less', 'cssmin', 'concat', 'uglify', 'lint', 'ftp-deploy:dev']);
+	grunt.registerTask('deploy-live', ['less', 'cssmin', 'concat', 'uglify', 'lint', 'shell:git-push', 'ftp-deploy:live']);
+}
